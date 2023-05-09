a02fdb9f42783f258ac28cc2e5a274798c542b4c
hfelizzola <ing.heriberto.felizzola@gmail.com>
2023-05-09 13:58:04 -0500

    prueba z

--- hellogit.py
+++ hellogit.py
@@ -4,14 +4,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from funciones import resumen
+from hipotesis import z_test
 
 #%load_ext autoreload
 #%autoreload 2
-# %%
+# %% histograma
 x = np.random.normal(loc=50, scale=3, size=1000)
 plt.hist(x, edgecolor="w", color="red")
 
-# %%
+# %% resumen estadístico
 resumen(x)
 
+# %% prueba de hipótesis
+z_test(x, mu=54, sigma=4)
 # %%
