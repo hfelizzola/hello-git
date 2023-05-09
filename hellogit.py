fc25265142da550da792799a385557a36d061ffd
hfelizzola <ing.heriberto.felizzola@gmail.com>
2023-05-08 23:15:42 -0500

    Esta es la primera versión del código

---
+++ hellogit.py
@@ -0,0 +1,7 @@
+# Archivo de prueba para probar git
+#%% cargar librerias
+import numpy as np
+import matplotlib.pyplot as plt
+# %%
+x = np.random.normal(loc=50, scale=3, size=1000)
+plt.hist(x, edgecolor="w")
